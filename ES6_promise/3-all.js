49da23325b03dfc581403b54bcc36b3f67afb2f6
jonapano <panojona1@gmail.com>
2025-07-30 18:22:02 +0200

    4th exercise

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from "./utils.js";
 
 function handleProfileSignup () {
     return Promise.all([uploadPhoto(), createUser()])
