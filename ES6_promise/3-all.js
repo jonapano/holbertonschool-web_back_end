aa0d1926a708ac37e023fdfc5d78ae4e894759b8
jonapano <panojona1@gmail.com>
2025-08-03 18:40:34 +0200

    eslint changes

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,13 +1,13 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup () {
-    return Promise.all([uploadPhoto(), createUser()])
-    .then((Response) => {
-        console.log (
-            `${Response[0].body} ${Response[1].firstName} ${Response[1].lastName}`
-        );
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((response) => {
+      console.log(
+        `${response[0].body} ${response[1].firstName} ${response[1].lastName}`,
+      );
     })
-    .catch(() => console.log("Signup system offline"));
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
---
+++ ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+export default function devideFunction(numerator, denominator) {
+    if (denominator === 0) {
+        throw new Error("Cannot divide by 0");
+    }
+    else {
+        return numerator / denominator;
+    }
+}
