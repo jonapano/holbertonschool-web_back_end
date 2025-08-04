0315538075f52dca308aab9d99038540312e5268
jonapano <panojona1@gmail.com>
2025-08-04 13:22:46 +0200

    fix eslint spaces errors

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,14 +1,13 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    let res;
+  const queue = [];
+  let res;
 
-    try {
-        res = mathFunction();
-    }
-    catch (error) {
-        res = error.toString();
-    }
-    queue.push(res);
-    queue.push("Guardrail was processed");
-    return queue;
+  try {
+    res = mathFunction();
+  } catch (error) {
+    res = error.toString();
+  }
+  queue.push(res);
+  queue.push('Guardrail was processed');
+  return queue;
 }
