08d955f3a1b6fd87a98f6bcfb38a136daa6934f9
jonapano <panojona1@gmail.com>
2025-09-09 15:42:46 +0200

    4th exercise

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,16 @@
+-- write a SQL script that creates a trigger that decreases the quantity of an item in the items table after an order is placed
+DROP TRIGGER IF EXISTS decrease_quantity;
+
+DELIMITER //
+
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+//
+
+DELIMITER ;
