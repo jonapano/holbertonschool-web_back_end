6b95f4d3bca48e9b5aba1e6b97917b575e4ebecd
jonapano <panojona1@gmail.com>
2025-09-25 14:34:34 +0200

    change 3-glam_rock

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,9 @@
--- List glam rock bands
-SELECT band_name, IFNULL(split, 2023) - formed as lifespan FROM metal_bands WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
+-- List glam rock bands with correct lifespan
+SELECT band_name,
+       CASE
+         WHEN split IS NULL THEN 0
+         ELSE split - formed + 1
+       END AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC;
