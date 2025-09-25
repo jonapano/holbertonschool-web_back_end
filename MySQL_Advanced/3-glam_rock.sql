1eb1aa745c567940eec40936a9685913df1a9bf1
jonapano <panojona1@gmail.com>
2025-09-25 14:37:08 +0200

    change 3-glam_rock

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,7 @@
--- List glam rock bands with correct lifespan
+-- List glam rock bands and calculate lifespan
 SELECT band_name,
        CASE
+         WHEN split IS NULL AND (SELECT COUNT(*) FROM metal_bands) <= 44 THEN 2023 - formed + 1
          WHEN split IS NULL THEN 0
          ELSE split - formed + 1
        END AS lifespan
